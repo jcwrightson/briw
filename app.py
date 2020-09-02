0d8416fe4c9171c9362e1eae4cf312b3064c5538
john <jcwrightson@gmail.com>
2020-09-02 12:55:33 +0100

    refactor and document

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 import os
 
 from classes import Person, Drink, Preference, Round
-from helpers import write_list, clear_screen, print_dict, print_list
+from helpers import read_lines, write_list, clear_screen, print_list, print_prefs
 
-
-def read_lines(file_name):
-    try:
-        the_file = open(file_name, "r")
-        lines = the_file.readlines()
-    except:
-        return []
-    return lines
+# ============================================
+# Define functions
+# ============================================
 
 
 def get_people():
+
+    # get lines of text from file as a list
     lines = read_lines("people.txt")
+
+    # Create an empty list
     data = []
+
+    # Iterate over each line
     for line in lines:
-        data.append(Person(line.strip()))
+
+        # Remove the `\n` char from the line leaving us just the text we want (a person's name)
+        name = line.strip()
+
+        # Create a `Person` object using this saved name
+        saved_person = Person(name)
+
+        # add this to our temp list
+        data.append(saved_person)
+
+    # Return the list of people back to the function caller
     return data
 
 
@@ -43,18 +54,36 @@ def save_and_exit():
     exit()
 
 
-people = get_people()
-drinks = get_drinks()
-prefs = {}
+def create_person(name: str):
+    # Create a new `Person` using the class constructor method
+    new_person = Person(name)
+
+    # Append this `Person` to our list of `people`
+    people.append(new_person)
 
 
-def create_person(name):
-    people.append(Person(name))
+def create_drink(name: str):
+    # Create a new `Drink` using the class constructor method
+    new_drink = Drink(name)
+
+    # Append this `Drink` to our list of `drinks`
+    drinks.append(new_drink)
 
 
-def create_drink(name):
-    drinks.append(Drink(name))
+def create_pref(person: Person, drink: Drink):
+    # A dictionary is a unique `key: value` data store
+    # Let's use the name of our person as the key, and the Preference object as the value
 
+    # e.g: "John" : { person: { name: "John" }, drink: { name: "Beer" } }
+    new_dict_item = {person.name: Preference(person, drink)}
+
+    # Update dictionary
+    prefs.update(new_dict_item)
+
+
+# ============================================
+# Instantiate variables
+# ============================================
 
 menu = """
 Please choose an option:
@@ -65,58 +94,103 @@ Please choose an option:
 [4] Create Person
 [5] Create Drink
 [6] Create Preference
-[7] Exit
+[0] Exit
 
 """
-exit_option = 7
+exit_option = 0
+people = get_people()
+drinks = get_drinks()
+prefs = {}
 
+
+# ============================================
+# Start App
+# ============================================
+
+
+# Create infinite loop
 while True:
-    option = 0
+
+    # Init `option` variable
+    option = None
+
     try:
+        # Prompt user to choose an option
         option = int(input(menu))
     except ValueError:
+        # If user entered a letter
         print('Please enter a number')
 
+    # If user entered a valid option we make it here
     if option == 1:
-        print_list(people, "People", "name")
+        print_list(people, "People", "name")  # Show list of ppl
     elif option == 2:
-        print_list(drinks, "Drinks", "name")
+        print_list(drinks, "Drinks", "name")  # Show list of drinks
     elif option == 3:
-        print_dict(prefs, "Preferences", "name")
+        print_prefs(prefs)  # Show list of prefs
     elif option == 4:
-        new_person = input("Please enter name: ")
-        create_person(new_person)
-        save_people()
+        name = input("Please enter name: ")
+        create_person(name)
+        save_people()  # Update saved ppl file (optional)
     elif option == 5:
-        new_drink = input("Please enter name: ")
-        create_drink(new_drink)
-        save_drinks()
+        name = input("Please enter name: ")
+        create_drink(name)
+        save_drinks()  # Update saved drinks file (optional)
     elif option == 6:
 
+        # Create Preference Object: { person: Person, drink: Drink }
+        # =============================================================
+        #
+        # 1. User needs to select a person. (Person)
+        # 2. User needs to select a drink. (Drink)
+        # 3. We need to put the two together. (Preference)
+        # 4. We need to update the local (in memory) dictionary (prefs)
+
+        # Init some variables to None
         selected_person = None
         selected_drink = None
 
+        # STEP #1
+        # =========================================
+        # Show user avail ppl in the people list
         print_list(people, "People", "name")
+
+        # While they haven't selected a valid option
         while selected_person == None:
             try:
-                person_id = int(input("Please choose a person: "))
-                selected_person = people[person_id]
+                # Prompt user to select a person's idx
+                person_idx = int(input("Please choose a person: "))
+                # Get that element from the person list
+                selected_person = people[person_idx]
             except:
+                # If user entered a letter, or chose an idx that doesn't exist
                 print("Invalid choice...")
 
+        # STEP #1
+        # =========================================
+        # Show user avail drinks in the drinks list
         print_list(drinks, "Drinks", "name")
 
+        # While they haven't selected a valid option
         while selected_drink == None:
             try:
-                drink_id = int(input("Please choose a drink: "))
-                selected_drink = drinks[drink_id]
+                # Prompt user to select a drink's idx
+                drink_idx = int(input("Please choose a drink: "))
+                # Get that element from the drink list
+                selected_drink = drinks[drink_idx]
             except:
+                # If user entered a letter, or chose an idx that doesn't exist
                 print("Invalid choice...")
 
-        new_pref = Preference(selected_person, selected_drink)
-        prefs.update({new_pref.person: new_pref.drink})
+        # STEP #3 and #4
+        # ==========================================
+        # Call create pref function to create a Preference and update dictionary
+        create_pref(selected_person, selected_drink)
 
     elif option == exit_option:
+        # Break out the loop if user selects `exit_option`
         break
 
+
+# Last thing we do before app terminates
 save_and_exit()
--- helpers.py
+++ helpers.py
@@ -1,15 +1,8 @@
 import os
 
+
 def clear_screen():
     os.system("clear")
-
-
-def print_dict(some_dict, title, prop):
-    clear_screen()
-    print(title.upper())
-    print('-'*30)
-    for key, val in some_dict.items():
-        print(getattr(key, prop), "\t"+getattr(val, prop))
 
 
 def print_list(some_list, title, prop):
@@ -22,39 +15,20 @@ def print_list(some_list, title, prop):
         idx += 1
 
 
-def read_data(file_name):
+def print_prefs(prefs):
+    clear_screen()
+    print("PREFERENCES")
+    print('-'*30)
+    for pref in prefs.values():
+        print(pref.person.name, "\t", pref.drink.name)
 
-    data = {}
-
+def read_lines(file_name):
     try:
         the_file = open(file_name, "r")
         lines = the_file.readlines()
-
-        for line in lines: # '0, Beer'
-
-            [key, val] = line.split(', ')  # ['0','Beer']
-            try:
-                data.update({int(key): val.strip()})
-            except:
-                data.update({key: val.strip()})
-
-        the_file.close()
-
-    except FileNotFoundError:
-        print(f"{file_name} not found")
-
-    return data
-
-
-def write_data(file_name, data):
-    try:
-        the_file = open(file_name, "w")
-
-        for key, val in data.items():
-            the_file.write(str(key) + ", " + val + "\n")
-    finally:
-        the_file.close()
-
+    except:
+        return []
+    return lines
 
 def write_list(file_name, the_list, prop):
     try:
