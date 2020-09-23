cda8708ea6b01ea5cd62ca05ab6cbc346db42041
John <jcwrightson@gmail.com>
2020-09-23 15:04:51 +0100

    convert dictionary into SQL

--- src/persist/db.py
+++ src/persist/db.py
@@ -2,7 +2,9 @@ import mysql.connector
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 def db(database: str):
     port = os.environ.get("mysql_port")
@@ -33,6 +35,18 @@ def update(mydb: object, sql: str, data: any):
         mydb.commit()
     except:
         print("Failed to update db")
+
+
+def update_with_dict(mydb: object, table:str, data: dict):
+
+    f = ", ".join(data.keys())
+    vals = ""
+    for i in range(0, len(data.keys())):
+        vals += "%s, "
+
+    sql = f"INSERT INTO {table} ({f}) VALUES({vals.strip(', ')})"
+
+    update(sql, data.values())
 
 
 def get_data(mydb: object, table: str, limit: int = 100):
--- tests/unit/test_get_data.py
+++ tests/unit/test_get_data.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import patch, Mock
 
 from src.core.core import print_table
-from src.persist.db import get_data
+from src.persist.db import get_data, update_with_dict
 
 
 class TestGetData(unittest.TestCase):
@@ -15,6 +15,18 @@ class TestGetData(unittest.TestCase):
 
         mock_db.cursor().execute.assert_called_with("SELECT * FROM test LIMIT 100")
         mock_db.cursor().fetchall.assert_called_once()
+
+    @patch("src.persist.db.update")
+    def test_update_with_dict(self, mock_update):
+
+        test_data = {"a": 1, "b": 2, "c": 3}
+        mock_db = Mock()
+        update_with_dict(mock_db, "test", test_data)
+
+        expectedSQL = "INSERT INTO test (a, b, c) VALUES(%s, %s, %s)"
+
+        self.assertEqual(mock_update.call_args.args[0], expectedSQL)
+
 
 
 if __name__ == "__main__":
